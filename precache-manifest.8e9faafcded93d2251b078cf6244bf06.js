ebae0e73425a7f2e28f08ec4385153e47744c158
xrizwan <xrizwanr@gmail.com>
2020-08-06 19:20:44 -0700

    Updates

---
+++ precache-manifest.8e9faafcded93d2251b078cf6244bf06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275331adc9c00ca4b86888be776349fb",
+    "url": "/TDD-Battleship/index.html"
+  },
+  {
+    "revision": "5f7733df73562216f317",
+    "url": "/TDD-Battleship/static/css/main.e852886d.chunk.css"
+  },
+  {
+    "revision": "4e1a91f89fb04754ccbd",
+    "url": "/TDD-Battleship/static/js/2.925d597c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TDD-Battleship/static/js/2.925d597c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f7733df73562216f317",
+    "url": "/TDD-Battleship/static/js/main.2708fe23.chunk.js"
+  },
+  {
+    "revision": "cb10d490cf6ff9d6684a",
+    "url": "/TDD-Battleship/static/js/runtime-main.123c5099.js"
+  }
+]);
